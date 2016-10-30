692a34f35b5f46c4e6c29572352d1f38568e2f78
unknown <>
2016-10-30 20:35:46 +0800

    load_data

--- js/common.js
+++ js/common.js
@@ -67,3 +67,44 @@ function query2obj(queryString) {
     }
     return obj;
 }
+
+function getDateDiff(dateTimeStamp){
+    var minute = 1000 * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var month = day * 30;
+    var now = new Date().getTime();
+    var diffValue = now - dateTimeStamp;
+    if ( diffValue < 0) { return; }
+    var monthC = diffValue / month;
+    var weekC = diffValue / (7 * day);
+    var dayC = diffValue / day;
+    var hourC = diffValue / hour;
+    var minC = diffValue / minute;
+    var result = "";
+    if(monthC >= 1){
+        result = parseInt(monthC) + "月前";
+    }
+    else if(weekC >= 1){
+        result = parseInt(weekC) + "周前";
+    }
+    else if(dayC >= 1){
+        result = parseInt(dayC) +"天前";
+    }
+    else if(hourC >= 1){
+        result = parseInt(hourC) +"小时前";
+    }
+    else if(minC >= 1){
+        result = parseInt(minC) +"分钟前";
+    }else
+        result = "刚刚";
+    return result;
+}
+
+function takeTimeApart(time){
+    var arr = time.split("T");
+    var date_str = arr[0].split("-");
+    var time_str = arr[1].split(":");
+    return {year: date_str[0], month: date_str[1], day: date_str[2],
+        hour: time_str[0], minute: time_str[1]}
+}
